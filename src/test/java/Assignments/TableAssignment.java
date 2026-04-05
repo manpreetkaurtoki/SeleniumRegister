7591820b4fd0353e363565e9c8832e75c8f11b88
Manpreet <manpreetkaurtoki@gmail.com>
2026-04-05 17:02:36 -0400

    Modify Testcase1, TestCase2. Added TestCase3

--- src/test/java/Assignments/TableAssignment.java
+++ src/test/java/Assignments/TableAssignment.java
@@ -12,6 +12,7 @@ public class TableAssignment extends Base {
 	By langCheck = By.xpath("//input[@name='lang' and @value='Java']");
 	By checkboxIntermediate = By.xpath("//input[@type='checkbox' and @value='Intermediate']");
 	By checkboxAdvanced = By.xpath("//input[@type='checkbox' and @value='Advanced']");
+	By dropdownLabel = By.xpath("//span[@class='dropdown-label']");
 
 	@Test
 	public void TestCase1() {
@@ -20,15 +21,12 @@ public class TableAssignment extends Base {
 		WebElement langFilterJava = driver.findElement(langCheck);
 		langFilterJava.click();
 
-		List<WebElement> listOfData = wait.until(ExpectedConditions
-				.visibilityOfAllElementsLocatedBy(By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]")));
+		List<WebElement> listOfData = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]//td[@headers='col_lang']")));
 		System.out.println(listOfData.size());
-
-		// table//tr//td[@headers='col_lang' and text()='Java']
 		for (WebElement element : listOfData) {
-			String language = element.findElement(By.xpath("//table//tr//td[@headers='col_lang']")).getText();
-			System.out.println(language);
-			Assert.assertEquals(language, "Java");
+			System.out.println(element.getText());
+			Assert.assertEquals(element.getText(), "Java");
 		}
 	}
 
@@ -38,14 +36,31 @@ public class TableAssignment extends Base {
 		driver.findElement(checkboxIntermediate).click();
 		driver.findElement(checkboxAdvanced).click();
 
-		List<WebElement> listOfData = wait.until(ExpectedConditions
-				.visibilityOfAllElementsLocatedBy(By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]")));
-		System.out.println(listOfData.size());
+		List<WebElement> listOfData = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]//td[@headers='col_level']")));
 
 		for (WebElement element : listOfData) {
-			String level = element.findElement(By.xpath("//table//tr//td[@headers='col_level']")).getText();
-			System.out.println(level);
-			Assert.assertEquals(level, "Beginner");
+			System.out.println(element.getText());
+			Assert.assertEquals(element.getText(), "Beginner");
+
 		}
+	}
+
+	@Test
+	public void TestCase3() {
+		driver.navigate().to("https://practicetestautomation.com/practice-test-table/?utm_source=chatgpt.com");
+		driver.findElement(dropdownLabel).click();
+		// wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//ul[@class='dropdown-menu']/li")));
+		WebElement enrolment = wait.until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//ul[@class='dropdown-menu']/li[text()='10,000+']")));
+		enrolment.click();
+		List<WebElement> listOfData = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+				By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]//td[@headers='col_enroll']")));
+		System.out.println(listOfData.size());
+		for (WebElement element : listOfData) {
+			int value = Integer.parseInt(element.getText().trim());
+			Assert.assertTrue(value >= 10000);
+		}
+
 	}
 }
